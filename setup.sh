a7da7e617f529230af29f0f888d5afa840f0bf52
git <adeelmalik78@gmail.com>
2025-09-05 17:24:42 -0500

    added some troubleshooting commands

--- setup.sh
+++ setup.sh
@@ -20,31 +20,31 @@ sudo docker run \
   liquibase-with-sqlplus:1.0 \
   --version
 
-# sudo docker run \
-#   --rm \
-#   --env LIQUIBASE_LICENSE_KEY=$LIQUIBASE_PRO_LICENSE_KEY \
-#   --env LIQUIBASE_SQLPLUS_PATH=/opt/oracle/instantclient_23_8/sqlplus \
-#   --env LIQUIBASE_LOG_LEVEL=FINE \
-#   -v ${PWD}/changelog:/liquibase/changelog \
-#   liquibase-with-sqlplus:1.0 \
-#   --url=jdbc:oracle:thin:@cs-oracledb.liquibase.net:1521/PP_DEV \
-#   --changeLogFile=changelog/db.changelog-master.xml \
-#   --username=liquibase_user \
-#   --password=liquibase_user \
-#   status
-
 sudo docker run \
   --rm \
   --env LIQUIBASE_LICENSE_KEY=$LIQUIBASE_PRO_LICENSE_KEY \
   --env LIQUIBASE_SQLPLUS_PATH=/opt/oracle/instantclient_23_8/sqlplus \
-  --env LIQUIBASE_LOG_LEVEL=INFO \
+  --env LIQUIBASE_LOG_LEVEL=FINE \
   -v ${PWD}/changelog:/liquibase/changelog \
   liquibase-with-sqlplus:1.0 \
   --url=jdbc:oracle:thin:@cs-oracledb.liquibase.net:1521/PP_DEV \
   --changeLogFile=changelog/db.changelog-master.xml \
-  --username=ADEEL \
-  --password=ADEEL#MALIK \
+  --username=liquibase_user \
+  --password=liquibase_user \
   status
+
+# sudo docker run \
+#   --rm \
+#   --env LIQUIBASE_LICENSE_KEY=$LIQUIBASE_PRO_LICENSE_KEY \
+#   --env LIQUIBASE_SQLPLUS_PATH=/opt/oracle/instantclient_23_8/sqlplus \
+#   --env LIQUIBASE_LOG_LEVEL=INFO \
+#   -v ${PWD}/changelog:/liquibase/changelog \
+#   liquibase-with-sqlplus:1.0 \
+#   --url=jdbc:oracle:thin:@cs-oracledb.liquibase.net:1521/PP_DEV \
+#   --changeLogFile=changelog/db.changelog-master.xml \
+#   --username=ADEEL \
+#   --password=ADEEL#MALIK \
+#   status
 
 
 # Troubleshooting commands

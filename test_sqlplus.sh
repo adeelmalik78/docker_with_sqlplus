f199152cb6b61eb69ed19b6faa7c0fae2a1ac0c5
git <adeelmalik78@gmail.com>
2025-09-08 10:41:18 -0500

    test_sqlplus.sh

--- test_sqlplus.sh
+++ test_sqlplus.sh
@@ -1,5 +1,5 @@
 cat > test_connect.sql << 'EOF' 
-connect ADEEL/"ADEEL#MALIK"@ccs-oracledb.liquibase.net:1521/PP_DEV
+connect ADEEL/"ADEEL#MALIK"@cs-oracledb.liquibase.net:1521/PP_DEV
 show user 
 exit 
 EOF
